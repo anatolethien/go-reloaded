1dd0e69733bacaf7af235e4de83d88591ebcef99
anatolethien <anatole.thien@ynov.com>
2021-07-10 21:08:16 +0200

    Join simplified

--- join.go
+++ join.go
@@ -5,10 +5,6 @@ func Join(elems []string, sep string) string {
 	if len(elems) == 0 {
 		return ""
 	}
-	if len(elems) == 1 {
-		return elems[0]
-	}
-
 	for i, s := range elems {
 		output += s
 		if i != len(elems)-1 {
